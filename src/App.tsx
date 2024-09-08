52ccbed5d60d41d575bfac3eb503536cb9164bcd
mausam-kumar <mausam_oct@outlook.com>
2024-09-08 18:03:57 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+
+  return (
+    <MantineProvider>
+      <div>
+        Assignment
+      </div>
+    </MantineProvider>
+  )
+}
+
+export default App
